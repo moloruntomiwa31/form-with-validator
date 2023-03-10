9dfc452f391e18c24f521b6d3544cd8fe13ffd48
moloruntomiwa31 <moloruntomiwa31@gmail.com>
2023-03-10 11:42:50 +0100

    Updated files

---
+++ README.md
@@ -0,0 +1,3 @@
+# Form with Validation.
+
+This is a form page with basic validation using Vanilla JS. ~
--- main.js
+++ main.js
@@ -1,11 +1,22 @@
-// popup
-
+// variable
 let payBtn = document.getElementById("open");
 let okBtn = document.getElementById("close");
 let popUp = document.querySelector(".thank-you");
 let inputEl = document.getElementsByTagName("input");
+
+const emailError = document.getElementById("email-error");
+const ccError = document.getElementById("cc-error");
+const cvvError = document.getElementById("cvv-error");
+
+let emailEl = document.getElementById("email");
+let ccnEl = document.getElementById('c-num');
+let cvvEl = document.getElementById("cvv");
+
+// popup
 payBtn.addEventListener("click", function() {
-    inputEl.value = ' ';
+    emailEl.value = '';
+    ccnEl.value = '';
+    cvvEl.value = '';
     popUp.classList.add("open")
 })
 okBtn.addEventListener("click", function() {
@@ -14,16 +25,37 @@ okBtn.addEventListener("click", function() {
 
 //validate
 
-class FormValidator {
-    constructor(form, field) {
-        this.form = form;
-        this.field = field;
+emailEl.addEventListener("keyup", function() {
+    let emailTxt = emailEl.value;
+    if (emailTxt.length == 0) {
+        emailError.innerHTML = "Mail required.";
+        return false
     }
+    emailError.innerHTML = `<img src="accept.png" width=16px>`
+})
 
-    initialise() {
-        
+ccnEl.addEventListener("keyup",  function() {
+    let ccnTxt = ccnEl.value;
+    if (ccnTxt.length == 0) {
+        ccError.innerHTML = "CCN required.";
+        return false
     }
-}
+    if (ccnTxt.length !== 16) {
+        ccError.innerHTML = "CCN required.";
+        return false
+    }
+    ccError.innerHTML =  `<img src="accept.png" width=16px>`
+})
 
-const formEl = document.getElementById("form");
-const fields = ["email", "c-num", "cvv"];
+cvvEl.addEventListener("keyup",  function() {
+    let cvvTxt = cvv.value;
+    if (cvvTxt.length == 0) {
+        cvvError.innerHTML = "cvv required";
+        return false
+    }
+    if (cvvTxt.length !== 4) {
+        cvvError.innerHTML = "cvv required";
+        return false
+    }
+    cvvError.innerHTML =  `<img src="accept.png" width=16px>`
+})
